2b33fc970738039954c72fd8663691fb64ced3dd
Samuel Gonzalez <xame2005@gmail.com>
2021-11-02 15:54:25 -0600

    Imprimir números pares

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,10 @@
-print ('hello world')
+var = 'mundo'
+
+print('Hello ' + var)
+
+contador = 0
+
+for i in range(0, 100):
+    contador = contador + 1
+    if(contador % 2 == 0):
+        print(contador)
